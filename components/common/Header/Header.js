1364d14ce8cb5f1ddae70adc99d2ff7d34a06d8f
sibiraj-002 <shansibi2002@gmail.com>
2024-09-16 17:17:27 +0530

    In header blog changed into blogss

--- components/common/Header/Header.js
+++ components/common/Header/Header.js
@@ -97,7 +97,7 @@ function Header() {
                                 {/* Blog Link */}
                                 <Col className="text-center" lg={1}>
                                     <Link href="/blog" className={`custom-header-style ${isActive('/blog') ? 'active-link' : ''}`}>
-                                        Blog
+                                        Blogss
                                     </Link>
                                 </Col>
                             </Row>
